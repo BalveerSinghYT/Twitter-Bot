b56186db077d7c00d214ebfb6008f6440d9a7584
Balveer Singh <balveersinghyt@gmail.com>
2021-05-19 20:55:02 +0530

    Update code.py

--- CopyPasteBot/code.py
+++ CopyPasteBot/code.py
@@ -1,30 +1,32 @@
 import tweepy
+from os import environ
 import time
 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+CONSUMER_KEY = environ['CONSUMER_KEY'] 
+CONSUMER_SECRET = environ['CONSUMER_SECRET'] 
+ACCESS_KEY = environ['ACCESS_KEY']      
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 # To get above keys apply for twitter developer account and make an app.
 
 def OAuth():
     try:
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
+        auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET )
+        auth.set_access_token(ACCESS_KEY,ACCESS_SECRET )
         return auth
     except Exception as e:
         return e
 
 auth = OAuth()
 api = tweepy.API(auth)
+user_name = 'balveersinghyt'
 
 while True:
-    user = api.get_user('balveersinghyt')  # Change 'balveersinghyt' to any other twitter username
+    user = api.get_user(user_name)  # Change 'subham_shweta' to any other twitter username
     copy = user.status.text
     try:
-        api.update_status(copy)         # Post copied tweet
-        print('Paste')
+        api.update_status("%s ~"%(copy,user_name))         # Post copied tweet
+        print('Pasted')
         time.sleep(5)
 
     except Exception as e:
